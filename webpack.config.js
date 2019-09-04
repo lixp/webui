a4d9a263de09f1551aa9033ff48fca45c921582d
lixp <lixp0915@163.com>
2019-09-04 18:29:44 +0800

    feat: babel es balabal .parse js$ test

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,22 @@ module.exports = {
         contentBase: __dirname + "\\src\\public",
         historyApiFallback: true,
         inline: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env","@babel/preset-react"
+                        ]
+                    }
+                },
+                exclude: /node_modules/
+            }
+        ]
     }
-
 };
 //注:__dirname是node.js中的一个全局变量,它指向当前执行脚本所在的目录
