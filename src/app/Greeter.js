0cd5570b71537675c8ce920eca67f1a13106a120
lixp <lixp0915@163.com>
2019-09-06 18:32:21 +0800

    feat: css module

--- src/app/Greeter.js
+++ src/app/Greeter.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import config from './config.json';
 
+import styles from './Greeter.css';
+
 class Greeter extends Component {
     render() {
-        return (<div>{config.greetText}</div>);
+        return (<div className={styles.root}>{config.greetText}</div>);
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,22 @@ module.exports = {
                     loader: "babel-loader"
                 },
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: {
+                                localIdentName: '[name]__[local]--[hash:base64:5]'
+                            }
+                        }
+                    }
+                ]
             }
         ]
     }
